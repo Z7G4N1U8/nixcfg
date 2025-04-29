2d73484232d1597ed3dae9dfef45959fce3ae657
peace <121939750+SomeEmptyBox@users.noreply.github.com>
2025-04-29 19:17:13 +0530

    home: gui: firefox: remove unnecessary extensions

--- homeModules/guiPrograms/firefox/extensions.nix
+++ homeModules/guiPrograms/firefox/extensions.nix
@@ -7,9 +7,6 @@
 {
   programs.firefox.profiles.default.extensions.packages =
     with inputs.firefox-addons.packages.${pkgs.system}; [
-      darkreader
-      decentraleyes
-      privacy-badger
       proton-pass
       proton-vpn
       stylus
