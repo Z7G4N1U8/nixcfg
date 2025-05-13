c4b44b47f061596f58d45750fd6c8843f8edb868
peace <121939750+SomeEmptyBox@users.noreply.github.com>
2025-05-13 10:23:28 +0530

    home: gui: ghostty: ux improvement

--- homeModules/guiPrograms/ghostty.nix
+++ homeModules/guiPrograms/ghostty.nix
@@ -2,15 +2,21 @@
   programs.ghostty = {
     enable = true;
     enableFishIntegration = true;
-    clearDefaultKeybinds = true;
     installBatSyntax = true;
 
     settings = {
       font-family = "JetBrainsMono NF SemiBold";
       font-size = 12;
+
       adjust-box-thickness = 1;
       adjust-cursor-thickness = 1;
+
       confirm-close-surface = false;
+      gtk-titlebar = false;
+
+      window-padding-x = 7;
+      window-padding-y = 7;
+      window-padding-balance = true;
     };
   };
 }
