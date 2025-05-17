66ee9bc747c628e0dd619ce0b4d027dd6dd379fb
peace <121939750+SomeEmptyBox@users.noreply.github.com>
2025-05-17 11:24:45 +0530

    system: add hyprland with flake packages

---
+++ system/hyprland.nix
@@ -0,0 +1,14 @@
+{
+  pkgs,
+  inputs,
+  ...
+}:
+
+{
+  programs.hyprland = {
+    enable = true;
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
+  };
+  environment.sessionVariables.NIXOS_OZONE_WL = 1;
+}
