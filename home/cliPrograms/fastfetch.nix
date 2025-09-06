9e335f41d6b88f75d118667b87aae5dc3543cd4f
peace <121939750+SomeEmptyBox@users.noreply.github.com>
2025-09-06 16:50:42 +0530

    update fastfetch config

--- home/cliPrograms/fastfetch.nix
+++ home/cliPrograms/fastfetch.nix
@@ -14,13 +14,13 @@
 
       display = {
         separator = " ";
-        # key.paddingLeft = 3;
         size.binaryPrefix = "si";
       };
 
       logo = {
         preserveAspectRatio = true;
-        source = "${user.assets}/logos/nix-cat.png";
+        source = "${user.assets}/logos/nix.png";
+        recache = true;
         type = "kitty";
         height = 9;
         padding.left = 1;
@@ -44,9 +44,10 @@
         }
 
         {
-          key = "󰅐 ";
-          type = "uptime";
-          keyColor = "green";
+          key = " ";
+          type = "DE";
+          keyColor = "cyan";
+          format = "{2} {3} (Wayland)";
         }
 
         {
@@ -55,23 +56,18 @@
           keyColor = "white";
         }
 
-        # {
-        #   key = " ";
-        #   keyColor = "blue";
-        #   type = "btrfs";
-        #   # format = "{1} / {2} ({3})";
-        # }
-
         {
-          key = " ";
-          type = "packages";
+          key = " ";
           keyColor = "red";
+          type = "btrfs";
+          format = "{5} / {7} ({8})";
         }
 
         {
-          key = " ";
-          type = "wm";
-          keyColor = "cyan";
+          key = " ";
+          type = "uptime";
+          keyColor = "green";
+          format = "{6} ({2}h, {3}m)";
         }
 
         {
