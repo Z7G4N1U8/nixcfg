d62964b51d5501dfc7c91af7096b320b21510045
peace <121939750+Z7G4N1U8@users.noreply.github.com>
2025-10-01 10:14:14 +0530

    chore(zen-browser): use quad9 dns

--- home/guiPrograms/zen-browser/settings.nix
+++ home/guiPrograms/zen-browser/settings.nix
@@ -5,7 +5,7 @@
     "browser.search.separatePrivateDefault" = false;
     "browser.contentblocking.category" = "strict";
     "network.trr.mode" = 3;
-    "network.trr.uri" = "https://firefox.dns.nextdns.io";
+    "network.trr.uri" = "https://dns.quad9.net/dns-query";
     "extensions.formautofill.creditCards.enabled" = false;
     "signon.rememberSignons" = false;
   };
