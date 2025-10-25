2e91bfd1f0032a20fb9fd601377aebe9ed56052c
peace <121939750+Z7G4N1U8@users.noreply.github.com>
2025-10-25 16:31:59 +0530

    add script to assets

--- system/scripts/moto-flasher.nix
+++ system/scripts/moto-flasher.nix
@@ -3,13 +3,7 @@
   ...
 }:
 let
-  src = pkgs.fetchFromGitHub {
-    owner = "SomeEmptyBox";
-    repo = "android_build_utils";
-    rev = "1639e5c014e602218d9421ac4a1a276e05275e62";
-    hash = "sha256-PqcCuKpTR/inGADt2N50pvlwtvSBDRa0ihhDtCJFMhY=";
-  };
-  flash = pkgs.writeShellScriptBin "flash" (builtins.readFile "${src}/scripts/flash.sh");
+  flash = pkgs.writeShellScriptBin "flash" (builtins.readFile ../../assets/scripts/moto-flasher.sh);
 in
 {
   environment.systemPackages = [ flash ];
