071dd841160a44b2941235b5bd3b0ba436a423e8
peace <git@z7g4n1u8.dev>
2025-12-28 18:16:29 +0530

    zen-browser: refactor addon installation using a function

--- home/guiPrograms/zen-browser/extensions.nix
+++ home/guiPrograms/zen-browser/extensions.nix
@@ -1,37 +1,17 @@
-{
-  programs.zen-browser.policies.ExtensionSettings = {
-
-    # Bitwarden Password Manager
-    "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-      install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+let
+  mkExt = name: id: private: {
+    name = id;
+    value = {
+      install_url = "https://addons.mozilla.org/firefox/downloads/latest/${name}/latest.xpi";
       installation_mode = "normal_installed";
-      private_browsing = true;
+      private_browsing = private;
     };
-
-    # Stylus
-    "{7a7a4a92-a2a0-41d1-9fd7-1e92480d612d}" = {
-      install_url = "https://addons.mozilla.org/firefox/downloads/latest/styl-us/latest.xpi";
-      installation_mode = "normal_installed";
-    };
-
-    # TWP - Translate Web Pages
-    "{036a55b4-5e72-4d05-a06c-cba2dfcc134a}" = {
-      install_url = "https://addons.mozilla.org/firefox/downloads/latest/traduzir-paginas-web/latest.xpi";
-      installation_mode = "normal_installed";
-    };
-
-    # uBlock Origin
-    "uBlock0@raymondhill.net" = {
-      install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-      installation_mode = "normal_installed";
-      private_browsing = true;
-    };
-
-    # Varia Integrator
-    "giantpinkrobots@protonmail.com" = {
-      install_url = "https://addons.mozilla.org/firefox/downloads/latest/varia-integrator/latest.xpi";
-      installation_mode = "normal_installed";
-    };
-
   };
+in
+{
+  programs.zen-browser.policies.ExtensionSettings = builtins.listToAttrs [
+    (mkExt "bitwarden-password-manager" "{446900e4-71c2-419f-a6a7-df9c091e268b}" true)
+    (mkExt "traduzir-paginas-web" "{036a55b4-5e72-4d05-a06c-cba2dfcc134a}" false)
+    (mkExt "ublock-origin" "uBlock0@raymondhill.net" true)
+  ];
 }
