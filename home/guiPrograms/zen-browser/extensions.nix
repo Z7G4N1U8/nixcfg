42ecd7fd32c9920b7874adb1d3fe67ee77236b62
peace <git@z7g4n1u8.dev>
2025-12-28 18:36:26 +0530

    zen-browser: add chrome mask extension

--- home/guiPrograms/zen-browser/extensions.nix
+++ home/guiPrograms/zen-browser/extensions.nix
@@ -11,6 +11,7 @@ in
 {
   programs.zen-browser.policies.ExtensionSettings = builtins.listToAttrs [
     (mkExt "bitwarden-password-manager" "{446900e4-71c2-419f-a6a7-df9c091e268b}" true)
+    (mkExt "chrome-mask" "chrome-mask@overengineer.dev" false)
     (mkExt "traduzir-paginas-web" "{036a55b4-5e72-4d05-a06c-cba2dfcc134a}" false)
     (mkExt "ublock-origin" "uBlock0@raymondhill.net" true)
   ];
